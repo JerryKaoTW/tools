d1bc3107970de838e68d0b6bb3d1a21a87870d14
shkao <terrencekao@gmail.com>
2024-12-13 11:19:31 +0800

    add some comment

--- getIP.php
+++ getIP.php
@@ -1,4 +1,5 @@
 <?php
+// This tool can get all IPs on local machine using ifconfig
 //exec("ifconfig | grep 'inet 10.' | awk '{print $2}'", $results);
 exec("ifconfig | egrep '\binet (addr:)?' | awk '!/127.0.0.1/{print $2}' | cut -d ':' -f 2", $results);
 print_r($results);
